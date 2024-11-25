dd7ca1414f083c06a051ca9cacd487113c77c76b
William Watson <wwatson@Williams-Laptop.local>
2024-11-25 09:03:25 -0500

    adding central getBreweries location

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -22,7 +22,6 @@
           :key="brewery.id"
           :routerLink="'/brewery/' + brewery.id"
         >
-          <!-- {{ brewery }} -->
           <ion-label>
             <h2>{{ brewery.name }}</h2>
             <p>{{ brewery.city + ", " + brewery.state }}</p>
@@ -52,8 +51,7 @@
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
-import axios from "axios";
+import { onMounted } from "vue";
 import {
   IonPage,
   IonHeader,
@@ -68,10 +66,9 @@ import {
   modalController,
   IonProgressBar,
 } from "@ionic/vue";
-
 import { beerOutline, search, starHalfOutline } from "ionicons/icons";
-
 import SearchBreweries from "./SearchBreweries.vue";
+import { useBreweries } from "@/data/useBreweries";
 
 export default {
   name: "BreweriesList",
@@ -89,42 +86,8 @@ export default {
     IonProgressBar,
   },
   setup() {
-    const breweries = ref([]); // List of loaded breweries
-    const page = ref(1); // Current page
-    const perPage = 50; // Items per page
-    const hasMoreData = ref(true); // Track if there are more breweries to load
-
-    // Fetch breweries data
-    const fetchBreweries = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.openbrewerydb.org/v1/breweries?page=${page.value}&per_page=${perPage}`
-        );
-
-        const fetchedBreweries = response.data;
-
-        // If no more data is returned, stop infinite scrolling
-        if (fetchedBreweries.length === 0) {
-          hasMoreData.value = false;
-        } else {
-          breweries.value = [...breweries.value, ...fetchedBreweries]; // Append new breweries
-        }
-      } catch (error) {
-        console.error("Error fetching breweries:", error);
-        hasMoreData.value = false; // Stop fetching on error
-      }
-    };
-
-    // Load more breweries when Infinite Scroll is triggered
-    const loadMoreBreweries = async (event) => {
-      if (hasMoreData.value) {
-        page.value += 1; // Increment page number
-        await fetchBreweries();
-      }
-
-      // Notify Infinite Scroll to complete
-      event.target.complete();
-    };
+    const { breweries, fetchBreweries, loadMoreBreweries, hasMoreData } =
+      useBreweries();
 
     async function openModal() {
       const modal = await modalController.create({
